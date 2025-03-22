b32f721b62ba427a5a04653cd1be3c4bbeab426d
Anabell <anabekk726@gmail.com>
2025-03-22 00:44:58 +0000

    Updated & finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+// 1 Create the arrays with different parts of the excuse: w
+  let who = ['My grandma ', 'The dog ', 'The mailman ', 'My bird '];
+  let action = ['broke ', 'ate ', 'crushed ', 'destroyed '];
+  let what = ['my car ', 'my homework ', 'my phone '];
+  let when = ['before class started.', 'while I was sleeping.', 'during lunchtime.'];
+
+//2. Randomly select 1 item each of the arrays
+  let selectedWho = Math.floor(Math.random()* who.length)
+  let selectedAction = Math.floor(Math.random()* action.length)
+  let selectedWhat = Math.floor(Math.random()* what.length)
+  let selectedWhen = Math.floor(Math.random()* when.length)
+
+ // console.log("selectedWho: ", selectedWho);
+ // console.log("selectedAction: ", selectedAction);
+ // console.log("selectedWhat: ", selectedWhat);
+ // console.log("selectedWhen: ", selectedWhen);
+
+//3. Combine the parts of the sentence to form the complete statement.
+  let statement = who[selectedWho] 
+                + action[selectedAction] 
+                + what[selectedWhat] 
+                + when[selectedWhen];
+
+  console.log(statement);
+
+  //4.  Apply the ramdon statement to the browser
+  let  htmlStatement = document.querySelector('.excuse');
+  htmlStatement.innerHTML = statement;
 };
